3cf42b8df2b8991c5be689ee1ac6aa4c7dc56c86
Martin <martinpugdal@live.dk>
2024-11-28 00:37:19 +0100

    forgot that for last commit 😅

---
+++ plugin/src/main/java/dk/martinersej/plugin/placeholderapi/FlawMinesPlaceholderExpansion.java
@@ -0,0 +1,52 @@
+package dk.martinersej.plugin.placeholderapi;
+
+import dk.martinersej.plugin.FlawMines;
+import dk.martinersej.plugin.placeholderapi.requests.ResetTimeRequest;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+public class FlawMinesPlaceholderExpansion extends PlaceholderExpansion {
+
+    private final FlawMines plugin;
+    private final PAPIRequest[] requests;
+
+    public FlawMinesPlaceholderExpansion(FlawMines plugin) {
+        this.plugin = plugin;
+
+        // load the requests here, maybe with reflection by the folder structure
+        this.requests = new PAPIRequest[]{
+            new ResetTimeRequest()
+        };
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return String.valueOf(plugin.getDescription().getAuthors());
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return plugin.getDescription().getName().toLowerCase();
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, @NotNull String params) {
+        for (PAPIRequest request : requests) {
+            if (request.matches(params)) {
+                return request.onRequest(player, params);
+            }
+        }
+        return null;
+    }
+}
